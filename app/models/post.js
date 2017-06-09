e63b9b6093868bf87d1f280c7d686bbf716f29cc
Drewster <Drewster>
2017-06-09 15:22:44 -0500

    initial commit, setup the skeleton

---
+++ app/models/post.js
@@ -0,0 +1,9 @@
+// app/models/post.js
+
+// load mongoose since we need it to define a model
+var mongoose = require('mongoose');
+
+module.exports = mongoose.model('post', {
+  text: String,
+  done: Boolean
+});
