f7f6bee3b908354dcf6bbea438119498f112997a
Drew McMinn <dmcminn@servicemanagement.com>
2017-06-14 11:16:43 -0500

    move shell start scripts to packages.json scripts section

--- start_browsersync.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-dir=$(cd $(dirname $0);echo $PWD)
-cd $dir
-npm start browser-sync start --proxy localhost:8888 --files "public/**/*.*"
--- start_nodemon.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-dir=$(cd $(dirname $0);echo $PWD)
-cd $dir
-npm start nodemon server.js
